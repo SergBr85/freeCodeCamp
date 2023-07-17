ec942a417f0d8c92b598df8345d204a2e73cf747
SergBr85 <you@example.comsergeybr@mail.ru>
2023-07-17 19:47:57 +0300

    task 6

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function timesFive(args) {
-	return args * 5;
+let a = [1, 2, 3, 4, 5];
+
+function nextInLine(arr, args) {
+	arr.push(args),
+	
+	console.log(arr)
+	console.log(args)
+	return arr.shift()
 }
-console.log(timesFive(5))
+console.log(nextInLine(a, 3))
