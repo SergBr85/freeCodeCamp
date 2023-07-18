92240442729cc0ccb6f7d7c80d209a9d6f6d321b
SergBr85 <you@example.comsergeybr@mail.ru>
2023-07-18 20:18:52 +0300

    task 13

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function century(year) {
-	return Math.ceil(year/100)
-  } //dupa
-console.log(century(2001))
+function countSheeps(arrayOfSheep) {
+	let sum = 0;
+	arrayOfSheep.forEach(value => { value? sum += 1:0})
+	return sum;
+};
+
+let array1 = [true,  true,  true,  false,
+	true,  true,  true,  true ,
+	true,  false, true,  false,
+	true,  false, false, true ,
+	true,  true,  true,  true ,
+	false, false, true,  true ];
+
+	console.log(countSheeps(array1))
